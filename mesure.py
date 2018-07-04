af1dcd4c720adfe5d93ccfc984c8a99c1294f603
Cyril Vincent <contact@cyrilvincent.com>
2018-07-04 14:32:49 +0200

    Numpy

---
+++ mesure.py
@@ -0,0 +1,25 @@
+import csv
+
+with open("mesure.csv") as f:
+    reader = csv.DictReader(f,delimiter=";")
+    theoricals = []
+    mesures = []
+    for row in reader:
+        theorical = float(row["good"])
+        mesure = float(row["bad"])
+        theoricals.append(theorical)
+        mesures.append(mesure)
+
+print(mesures)
+
+import numpy as np
+
+t = np.array(theoricals)
+m = np.array(mesures)
+diff = m - t
+print(diff)
+quadratic = np.square(diff)
+print(quadratic)
+
+
+
